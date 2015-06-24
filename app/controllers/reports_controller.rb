c5f60d26893dc12fbe932b661fd512a7bf648979
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-24 04:12:25 -0400

    moved all_data view into report mailer view and method into report mailer controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,13 @@
 class ReportsController < ApplicationController
   def all_data
-    @assembly = Assembly.find_by_name(params[:name])
-    @hits = Hit.where(
-    subject_id: Gene.where(
-    sequence_id: Sequence.where(
-    assembly_id: Assembly.where(
-    name: params[:name]))).order(percent_similarity: :desc)
+    @email = params[:email]
+    
+
+    # @assembly = Assembly.find_by_name(params[:name])
+    # @hits = Hit.where(
+    # subject_id: Gene.where(
+    # sequence_id: Sequence.where(
+    # assembly_id: Assembly.where(
+    # name: params[:name]))).order(percent_similarity: :desc)
   end
 end
